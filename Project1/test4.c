6ae33f88ce5d90d3c1d58f1b3c91d7e19f0455e1
Weilai <futuretoo@126.com>
2018-11-17 21:45:33 +0800

    共用体赋值

--- Project1/test4.c
+++ Project1/test4.c
@@ -27,15 +27,23 @@ struct Student {
 void main() {
 	void printInfo(struct Person s);
 
-	struct Person a = {1,1,"male",18,"Jack",'s',95.5};
+	struct Person a = {1,1,"male",18,"Jack",'s',95.5};//98.5默认复制给第一个属性值
 //	a.grade.gradeC = 98.5;
 
-	//union如果不指定位置，默认是赋值给第一个属性，即grade.gradeC 
+	//union如果不指定位置，默认是赋值给第一个属性，即grade.gradeC
 	struct Person b = {2,1,"female",16,"Sherry",'t',.grade.gradeT='C'};
 
 
 	printInfo(a);
 	printInfo(b);
+
+//	union data d = {98.5,'a'};//这么赋值是错误的，因为他们占用同一个存储单元
+//只能对第一个或指定的一个属性赋值，后来的属性会覆盖前面的
+	union data c = {97.5};
+	printf("%.2f\n",c.gradeC);
+	printf("%d\n",c.gradeC);//这里打印0，说明float不能直接打印d 
+	printf("%c\n",(int)c.gradeC);
+
 }
 
 void printInfo(struct Person s) {
@@ -45,12 +53,14 @@ void printInfo(struct Person s) {
 	printf("age:%d\n",s.age);
 	printf("sex:%s\n",s.sex);
 	printf("class:%d\n",s.class);
-	//字符串不能直接对比，只能用strcmp函数 
+
+	//字符串不能直接对比，只能用strcmp函数
+	//字符可以对比
 	if(s.job=='s') {
 		printf("grade:%.2f\n",s.grade.gradeC);
 	} else
 		printf("grade:%c\n",s.grade.gradeT);
-	
+
 	printf("\n");
 }
 
